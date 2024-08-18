7e27781fdd7d347f425aacfab8714c12d274ef20
elham-qolizade <168982184+elham-qolizade@users.noreply.github.com>
2024-08-18 17:25:03 +0330

    Complete API

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Cart from "./component/Cart";
+
+function App() {
+  return (
+    <>
+      <Cart />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/component/Cart.tsx
@@ -0,0 +1,43 @@
+import addTocart from "../assets/icon-add-to-cart.svg";
+import React, { useEffect, useState } from "react";
+
+interface Item {
+  Image: {
+    thumbnail: string;
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  name: string;
+  category: string;
+  price: number;
+}
+
+const Cart: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:10000/")
+      .then((response) => response.json())
+      .then((data: Item[]) => setItems(data));
+  }, []);
+
+  return (
+    <>
+      {items.map((item) => (
+        <div key={item.name}>
+          <img src={item.Image.desktop} alt={item.name} />{" "}
+          <button className="flex px-6 py-2 border-brown-500 border-rounded-3xl">
+            <img src={addTocart} alt="" />
+            Add to Cart
+          </button>
+          <p className="text-brown-500">{item.category}</p>
+          <h2 className="text-brown-900">{item.name}</h2>
+          <p className="font-semibold text-red-500">${item.price}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Cart;
