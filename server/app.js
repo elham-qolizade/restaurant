7c08f05211bb1ce60d57095d9dd34b2df7d79896
elham-qolizade <168982184+elham-qolizade@users.noreply.github.com>
2024-10-17 14:14:24 +0330

    edit code

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { faker } = require('@faker-js/faker');
+const { faker } = require('https://apilist.fun/company/kkk');
 const app = express();
-const port = 2043;
+const port = 1044;
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
                 "en": "Waffle",
                 "fa": "وافل"
             },
-          
+
             "price": 6.50,
             "image": faker.image.food()
         },
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
                 "en": "Crème Brûlée",
                 "fa": "کرم بروله"
             },
-            
+
             "price": 7.00,
             "image": faker.image.food()
         },
@@ -54,7 +54,7 @@ app.get('/', (req, res) => {
                 "en": "Tiramisu",
                 "fa": "تیرامیسو"
             },
-          
+
             "price": 5.50,
             "image": faker.image.food()
         },
@@ -67,62 +67,62 @@ app.get('/', (req, res) => {
                 "en": "Baklava",
                 "fa": "باقلوا"
             },
-        
+
             "price": 4.00,
-            "image": faker.image.food() 
-         },
-   {
-    "name":{
-        "en": "Lemon Meringue Pie",
-        "fa":"پای مرنگ لیمویی"
-      },
-      "category": {
-        "en": "pie",
-        "fa": "پای"
-    },
-         "price": 5.00,
-         "image": faker.image.food() 
-      },
-  {
-    "name":{
-        "en": "Red Velvet Cake",
-        "fa":"کیک قرمز مخملی"
-      },
-      "category": {
-        "en": "cake",
-        "fa": "کیک"
-    },
+            "image": faker.image.food()
+        },
+        {
+            "name": {
+                "en": "Lemon Meringue Pie",
+                "fa": "پای مرنگ لیمویی"
+            },
+            "category": {
+                "en": "pie",
+                "fa": "پای"
+            },
+            "price": 5.00,
+            "image": faker.image.food()
+        },
+        {
+            "name": {
+                "en": "Red Velvet Cake",
+                "fa": "کیک قرمز مخملی"
+            },
+            "category": {
+                "en": "cake",
+                "fa": "کیک"
+            },
 
-    "price": 4.50,
-    "image": faker.image.food() 
- },
-{
-    "name":{
-        "en": "Salted Caramel Brownie",
-        "fa":"براونی کاراملی شور"
-      },
-      "category": {
-        "en": "Brownie",
-        "fa": "براونی"
-    },
- 
-    "price": 4.50,
-    "image": faker.image.food() 
- },
-{
- "name":{
-   "en": "Vanilla Panna Cotta",
-   "fa":"پاناکوتا وانیلی"
- },
- "category": {
-    "en": "Panna Cotta",
-    "fa": "پاناکوتا"
-},
+            "price": 4.50,
+            "image": faker.image.food()
+        },
+        {
+            "name": {
+                "en": "Salted Caramel Brownie",
+                "fa": "براونی کاراملی شور"
+            },
+            "category": {
+                "en": "Brownie",
+                "fa": "براونی"
+            },
 
-        "price": 6.50,
-    "image": faker.image.food() 
-     },
-     
+            "price": 4.50,
+            "image": faker.image.food()
+        },
+        {
+            "name": {
+                "en": "Vanilla Panna Cotta",
+                "fa": "پاناکوتا وانیلی"
+            },
+            "category": {
+                "en": "Panna Cotta",
+                "fa": "پاناکوتا"
+            },
+
+            "price": 6.50,
+            "image": faker.image.food()
+        },
+
     ];
 
     const productsWithDualLanguage = products.map(product => {
@@ -137,7 +137,7 @@ app.get('/', (req, res) => {
 
     res.json(productsWithDualLanguage);
 });
- 
+2
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
 });
